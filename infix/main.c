0f50c17bf257786eca36b9510d7ee8fccd98dfce
KYSandeep <sandeep.somy@gmail.com>
2016-05-22 01:17:30 +0530

    added comments explaining the working of the funcion

--- infix/main.c
+++ infix/main.c
@@ -7,15 +7,15 @@ int main(){
 	char str[128], c;
 	int i;
 	for(i = 0; i < 128;){ 
-		scanf("%c", &c);
-		if(c == 10){
+		scanf("%c", &c);		// scans the char
+		if(c == 10){			//if enter is pressed, it breaks
 			str[i] = '\0';
 			break;
 		}
-		else if(c == 32)
+		else if(c == 32)		//if space is pressed, it skips the scanned input
 			continue;
 		
-		else{
+		else{				//otherwise it adds the input to the string
 			str[i] = c;
 			i++;
 		}
